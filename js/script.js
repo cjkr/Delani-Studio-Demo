afe2e8f694bf59554b8cd5eef94e7801ce8ab78b
Japheth Rono <japheth.rono@student.moringaschool.com>
2024-07-12 01:07:17 +0300

    phase 1 of what-we-do and portfolio

--- js/script.js
+++ js/script.js
@@ -13,4 +13,50 @@ $(document).ready(function () {
       alert("Please fill out all fields before submitting.");
     }
   });
+
+  // Toggle description for "What We Do" items
+  $(".what-we-do-icon").on("click", function () {
+    var description = $(this).data("description");
+    $("#what-we-do-description").text(description);
+  });
+
+  // Hover effect for portfolio items
+  $(".portfolio-item").hover(
+    function () {
+      $(this).find(".portfolio-hover").fadeIn(200);
+    },
+    function () {
+      $(this).find(".portfolio-hover").fadeOut(200);
+    }
+  );
+
+  // Dynamically generate portfolio items
+  var portfolioContainer = $("#portfolio-items");
+  var portfolioImages = [
+    "work1.jpg",
+    "work2.jpg",
+    "work3.jpg",
+    "work4.jpg",
+    "work5.jpg",
+    "work6.jpg",
+    "work7.jpg",
+    "work8.jpg",
+  ];
+
+  portfolioImages.forEach(function (image, index) {
+    var col = $("<div>", {
+      class: "col-md-3 portfolio-item position-relative",
+    });
+    var img = $("<img>", {
+      src: "assets/portfolio/" + image,
+      alt: "Project " + (index + 1),
+      class: "img-fluid",
+    });
+    var hoverDiv = $("<div>", { class: "portfolio-hover text-center" }).text(
+      "Project " + (index + 1)
+    );
+
+    col.append(img).append(hoverDiv);
+    portfolioContainer.append(col);
+  });
 });
